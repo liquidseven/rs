df955464f765a7eb3682f21f00d3daf980f3b577
Matthew Young <mnyoung47@gmail.com>
2018-05-14 08:26:40 -0500

    Resume updated

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder = "C:\\Users\\Matt\\Desktop\\resume")
+
 
 @app.route('/')
-def index:
-    return render_template('resume.html')
+def index():
+    return render_template('index.html')
 
-if __name__name = '__main__':
-    app.run()
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
